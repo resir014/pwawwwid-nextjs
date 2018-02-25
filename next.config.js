2ace9d383e24c58b6238cf4c0899e348a0c115ca
Resi Respati <resir014@gmail.com>
2018-02-25 17:20:52 +0700

    replace next-workbox-webpack-plugin with next-offline, added export config

--- next.config.js
+++ next.config.js
@@ -1,27 +1,61 @@
+const withOffline = require('next-offline')
 const withTypeScript = require('@zeit/next-typescript')
 const {
   WebpackBundleSizeAnalyzerPlugin,
 } = require('webpack-bundle-size-analyzer')
-const NextWorkboxWebpackPlugin = require('@pwa/next-workbox-webpack-plugin')
 
-module.exports = withTypeScript({
-  webpack: (config, { isServer, dev, buildId, config: { distDir } }) => {
-    if (isServer || dev) {
+module.exports = withTypeScript(
+  withOffline({
+    dontAutoRegisterSw: true,
+    exportPathMap: async () => {
+      const response = await fetch(
+        'https://medium.com/wwwid/latest?format=json',
+      )
+      const body = JSON.parse(response.substr(response.indexOf('{')))
+
+      const pages = body.payload.posts.reduce((pages, post) =>
+        Object.assign({}, pages, {
+          [`/p/${post.id}`]: {
+            page: '/post',
+            query: { id: post.id },
+          },
+        }),
+      )
+
+      return Object.assign({}, pages, {
+        '/': { page: '/' },
+      })
+    },
+    webpack: (config, { isServer, dev, buildId, config: { distDir } }) => {
+      if (isServer || dev) {
+        return config
+      }
+
+      if (process.env.NPM_CONFIG_REPORT) {
+        config.plugins.push(new WebpackBundleSizeAnalyzerPlugin('stats.txt'))
+      }
+
       return config
-    }
-
-    config.plugins.push(
-      new NextWorkboxWebpackPlugin({
-        distDir,
-        buildId,
-        precacheManifest: true,
-      }),
-    )
-
-    if (process.env.NPM_CONFIG_REPORT) {
-      config.plugins.push(new WebpackBundleSizeAnalyzerPlugin('stats.txt'))
-    }
-
-    return config
-  },
-})
+    },
+    workboxOpts: {
+      runtimeCaching: [
+        {
+          urlPattern: /.png$/,
+          handler: 'staleWhileRevalidate',
+        },
+        {
+          urlPattern: /api\.json/,
+          handler: 'staleWhileRevalidate',
+          options: {
+            cacheableResponse: {
+              statuses: [0, 200],
+              headers: {
+                'x-test': 'true',
+              },
+            },
+          },
+        },
+      ],
+    },
+  }),
+)
